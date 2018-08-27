ddaa5fd7adcc0ac3279a36b6872169853de0a7a6
anneprt <anne.parrot82@gmail.com>
2018-08-27 22:48:59 +0200

    Maintenant on choisit la marque puis on affiche les modèles correspondants

--- Voitures/Program.cs
+++ Voitures/Program.cs
@@ -11,48 +11,78 @@ namespace Voitures
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Modèles chez Renault");
+            Console.WriteLine("VOITURES");
+            Console.WriteLine();
 
-            AfficherModeles();
+            int idMarque = ChoisirMarque();
+            AfficherModeles(idMarque);
 
             Console.ReadKey();
-            
+
         }
 
-        private static void AfficherModeles()
+        private static int ChoisirMarque()
         {
             var connexion = CreerConnexion();
             connexion.Open();
 
             var commande = connexion.CreateCommand();
-            //commande.CommandText = "SELECT Nom FROM Modeles WHERE IdMarque=2";
-            commande.CommandText =
-                @"SELECT M.Nom AS NonModele,S.Nom AS NomSegment
-                FROM Modeles M
-                    INNER JOIN Segments S ON S.Id=M.IdSegment
-                WHERE IdMarque=2";
-        
+
+            commande.CommandText = "SELECT * FROM Marques ORDER BY Nom";
             var dataReader = commande.ExecuteReader();
+
             while (dataReader.Read())
             {
-                // var colonneNom = dataReader.GetOrdinal("Nom");
-                //Console.WriteLine(dataReader.GetString(colonneNom));
-                var indexColonneNomModele = dataReader.GetOrdinal("NomModele");
-                var indexColonneNomSegment = dataReader.GetOrdinal("NomSegment");
-                Console.Write(dataReader.GetString(indexColonneNomModele));
+
+                var indexColonneNom = dataReader.GetOrdinal("Nom");
+                var indexColonneId = dataReader.GetOrdinal("Id");
+                Console.Write(dataReader.GetString(indexColonneNom));
                 Console.Write("(");
-                Console.Write(dataReader.GetString(indexColonneNomSegment));
+                Console.Write(dataReader.GetInt32(indexColonneId));
                 Console.WriteLine(")");
             }
 
             connexion.Close();
 
-        }
-        static SqlConnection CreerConnexion()
-        {
-            var chaineConnexion = "Server=.;database=voitures;Trusted_Connection=True";
-           return new SqlConnection(chaineConnexion);
+            Console.WriteLine("Quelle marque (Id)?");
+            return int.Parse(Console.ReadLine());
         }
 
-    }
-}
+            private static void AfficherModeles(int idMarque)
+            {
+                Console.WriteLine("Modèles");
+                Console.WriteLine();
+
+                var connexion = CreerConnexion();
+                connexion.Open();
+
+                var commande = connexion.CreateCommand();
+                commande.CommandText =
+                    @"SELECT M.Nom AS NonModele,S.Nom AS NomSegment
+                FROM Modeles M
+                    INNER JOIN Segments S ON S.Id=M.IdSegment
+                WHERE IdMarque=@IdMarque";
+            commande.Parameters.AddWithValue("@IdMarque", idMarque);
+
+                var dataReader = commande.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    var indexColonneNomModele = dataReader.GetOrdinal("NomModele");
+                    var indexColonneNomSegment = dataReader.GetOrdinal("NomSegment");
+                    Console.Write(dataReader.GetString(indexColonneNomModele));
+                    Console.Write("(");
+                    Console.Write(dataReader.GetString(indexColonneNomSegment));
+                    Console.WriteLine(")");
+                }
+
+                connexion.Close();
+
+            }
+            static SqlConnection CreerConnexion()
+            {
+                var chaineConnexion = "Server=.;database=voitures;Trusted_Connection=True";
+                return new SqlConnection(chaineConnexion);
+            }
+
+        }
+    } 
