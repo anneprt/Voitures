df3b735b254542230369f5b40390e26460d271f6
anneprt <anne.parrot82@gmail.com>
2018-08-27 22:21:34 +0200

    Liste des modèles Renault (Id = 2)

--- Voitures/Program.cs
+++ Voitures/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,21 @@ namespace Voitures
     {
         static void Main(string[] args)
         {
+            var chaineConnexion = "Server=.;database=voitures;Trusted_Connection=True";
+            var connexion = new SqlConnection(chaineConnexion);
+            connexion.Open();
+
+            var commande = connexion.CreateCommand();
+            commande.CommandText = "SELECT Nom FROM Modeles WHERE IdMarque=2";
+            var dataReader = commande.ExecuteReader();
+            while (dataReader.Read())
+            {
+                var colonneNom = dataReader.GetOrdinal("Nom");
+                Console.WriteLine(dataReader.GetString(colonneNom));
+            }
+
+            connexion.Close();
+            Console.ReadKey();
         }
     }
 }
