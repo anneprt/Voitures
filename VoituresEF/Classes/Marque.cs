2a8417fc0a05f36588e80de74a2194ee9d057c47
anneprt <anne.parrot82@gmail.com>
2018-08-28 22:30:11 +0200

    Modification table Marques Nouveau nom: Marque Champ Id -> IdMarque Champ Nom -> Libelle

--- VoituresEF/Classes/Marque.cs
+++ VoituresEF/Classes/Marque.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace VoituresEF.Classes
 {
+    [Table("Marque")]
+
     public class Marque
     {
+        [Column("IdMarque")]
         public int Id { get; set; }
+
+        [Column("Libelle")]
         public string Nom { get; set; }
     }
 }
