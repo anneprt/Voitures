eb31a91b912071e8f18e517a1f662dca7722944b
anneprt <anne.parrot82@gmail.com>
2018-08-28 22:22:43 +0200

    Simplification de la classe Contexte

--- VoituresEF/Data/Contexte.cs
+++ VoituresEF/Data/Contexte.cs
@@ -10,11 +10,7 @@ namespace VoituresEF.Data
 {
     public class Contexte : DbContext
     {
-        public Contexte()
-            : base("Contexte")
-        {
-
-        }
+        
         public DbSet<Marque> Marques { get; set; }
         public DbSet<Modele> Modeles { get; set; }
         public DbSet<Segment> Segments { get; set; }
